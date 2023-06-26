f03651511ebd4d684ae55eef8d786fa92ee701cd
CodeWithkaran02 <7870karuna@gmail.com>
2023-06-26 18:56:14 +0530

    script ready to deploy

---
+++ script.js
@@ -0,0 +1,10 @@
+let addbtn = document.querySelector(".add-btn");
+let modal = document.querySelector(".modal-cont");
+
+let addModal = true;
+
+addbtn.addEventListener("click", function () {
+
+    model.style.display = "flex";
+    console.log("click the button ")
+})
